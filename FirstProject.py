0b6ae493ad96476f998743b14268173801176edf
Murad <binnetzadmurad@gmail.com>
2024-04-14 22:39:54 +0200

    Simple rock paper scissors game in python
    It's my first project, wanted to see, how can i use my knowledge of basics in python

--- FirstProject.py
+++ FirstProject.py
@@ -1,10 +1,12 @@
 import random
 print("This is a simple 'rock paper scissors' game.")
 print("Let's start!")
-gametype = int(input("You want to play regular or best of 3? '1' for regular, '2' for best of 3: "))
+gametype = None
+while gametype != 1 or gametype != 2:
+    gametype = int(input("You want to play regular or best of 3? '1' for regular, '2' for best of 3: "))
 
 
-def user():
+def user():  # Get the user's choise
     userturn = input("Enter your choise rock/paper/scissors: ").lower()
     while userturn not in ['rock', 'paper', 'scissors']:
         print("Invalid choise!")
@@ -12,12 +14,12 @@ def user():
     return userturn
 
 
-def computer():
+def computer():  # Computer's choise
     computerturn = random.choice(["rock", "paper", "scissors"])
     return computerturn
 
 
-def getwinner(userturn, computerturn):
+def getwinner(userturn, computerturn):  # Get the winner
     global winner
     winner = None
     if (userturn == 'rock' and computerturn == 'scissors') or \
@@ -32,33 +34,33 @@ def getwinner(userturn, computerturn):
         winner = "computer"
 
 
-def getgametype():
+def getgametype():  # Choose the game type
     if gametype == 1:
         startgame()
 
     elif gametype == 2:
         userwincount = 0
         pcwincount = 0
-        while userwincount <= 2 or pcwincount <= 2:
+        while userwincount < 2 and pcwincount < 2:
             startgame()
             if winner == "user":
                 userwincount += 1
             elif winner == "computer":
                 pcwincount += 1
         else:
-            if userwincount == 3:
+            if userwincount == 2:
                 print(f"You won! It's {userwincount} - {pcwincount}.")
-            elif pcwincount == 3:
+            elif pcwincount == 2:
                 print(f"You lost! It's {userwincount} - {pcwincount}.")
 
 
-def startgame():
+def startgame():  # Starting the game
     userturn = user()
     computerturn = computer()
     print(f"Your choise is {userturn}")
     print(f"Computer choise is {computerturn}")
 
-    winner(userturn, computerturn)
+    getwinner(userturn, computerturn)
 
 
 getgametype()
